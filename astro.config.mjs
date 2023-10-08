5774a981161928c6b4d11e51675db39364b5fb93
Hollis Wu <Holi0317@users.noreply.github.com>
2023-10-09 03:05:53 +0800

    Scaffold and setup astro (#3)
    * Add mdx plugin
    
    * Add unocss
    
    * Add icons to unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,32 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import UnoCSS from "unocss/astro";
+
+const DEV_PORT = 4321;
+
+function getSite() {
+  // Site URL defined by Cloudflare build
+  // Ref: https://developers.cloudflare.com/pages/platform/build-configuration/#environment-variable
+  if (process.env.CF_PAGES_URL) {
+    return process.env.CF_PAGES_URL;
+  }
+
+  // Fallback to local dev
+  return `http://localhost:${DEV_PORT}`;
+}
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: getSite(),
+
+  server: {
+    port: DEV_PORT,
+  },
+
+  integrations: [
+    mdx(),
+    UnoCSS({
+      injectReset: true,
+    }),
+  ],
+});
