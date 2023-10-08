ed4b54c0d6c961ef8c283ba3288e4cb0e9c2975b
Hollis Wu <whyhollis@gmail.com>
2023-10-08 14:47:57 -0400

    Add mdx plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+const DEV_PORT = 4321;
+
+function getSite() {
+  // Site URL defined by Cloudflare build
+  // Ref: https://developers.cloudflare.com/pages/platform/build-configuration/#environment-variable
+  if (process.env.CF_PAGES_URL) {
+    return process.env.CF_PAGES_URL;
+  }
+
+  // Fallback to local dev
+  return `http://localhost:${DEV_PORT}`;
+}
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: getSite(),
+
+  server: {
+    port: DEV_PORT,
+  },
+
+  integrations: [mdx()],
+});
