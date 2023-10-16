1ceb6c8c168815a1151e2a21c641019a642fbd48
Hollis Wu <Holi0317@users.noreply.github.com>
2023-10-16 22:52:17 +0800

    Add about page (#11)
    * Move blog to /blog path
    
    * Add about page
    
    * Add links on about page

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -1,5 +1,6 @@
 export const AppConfig = {
   name: "Hollis Wu",
+  email: "hi@h4s.one",
   links: {
     github: "https://github.com/Holi0317",
     repo: "https://github.com/Holi0317/blog.h4s.one/",
