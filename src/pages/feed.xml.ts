547b4d1ac012e906350c608bb06d1c63019e7ac8
Hollis Wu <Holi0317@users.noreply.github.com>
2023-10-16 23:34:06 +0800

    Add RSS support (#14)
    * Add RSS support
    
    Closes #5
    
    * Filter out drafts in rss

---
+++ src/pages/feed.xml.ts
@@ -0,0 +1,36 @@
+import { DateTime } from "luxon";
+import type { APIContext } from "astro";
+import rss from "@astrojs/rss";
+import { AppConfig } from "../AppConfig";
+import { getCollection } from "astro:content";
+import { urlForBlog } from "../utils/blog";
+
+export async function GET(context: APIContext) {
+  if (context.site == null) {
+    throw new Error("site is not configured in astro.config.mjs");
+  }
+
+  const blog = await getCollection("blog");
+
+  return rss({
+    title: `${AppConfig.name} blog`,
+    description: `Random thoughts from ${AppConfig.name}`,
+    site: context.site,
+    // RSS Spec: https://www.rssboard.org/rss-specification
+    // TODO: Add pubDate
+    customData: `
+    <language>en-us</language>
+    <generator>${context.generator}</generator>
+    <lastBuildDate>${DateTime.utc().toHTTP()}</lastBuildDate>
+    `,
+
+    items: blog
+      .filter((blog) => !blog.data.draft)
+      .map((blog) => ({
+        title: blog.data.title,
+        pubDate: blog.data.pubDate,
+        description: blog.data.snippet,
+        link: urlForBlog(blog),
+      })),
+  });
+}
